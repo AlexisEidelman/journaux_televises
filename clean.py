53b5caa20724cd3d7f36e480c58064428bea1051
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-10-24 13:22:51 +0200

    new data

--- clean.py
+++ clean.py
@@ -8,31 +8,37 @@ Created on Mon Oct 17 19:45:11 2016
 import os
 import pandas as pd
 
-file_name = 'data/durees.csv'
+file_name = 'data/new_durees.csv'
 if not os.path.exists(file_name):
     import download
 
 data = pd.read_csv(file_name, encoding='cp1252', sep=';',
                    skiprows=1)
 
-# une ligne en trop
-data.drop(168, inplace=True)
+data.columns = ['date', 'sujet'] + data.columns.tolist()[2:]
+# une ligne en trop dans la premiere version
+# data.drop(168, inplace=True)
 
 # on transforme les durées en seconde
 for col in data.columns[2:]:
     data[col] = pd.to_timedelta(data[col])/pd.Timedelta('1s')
 
-# a finir mais peut-être de nouvelles données viendront...
-date = data.iloc[:,0]
-count_by_date = date.value_counts()
-a_regarder = count_by_date[count_by_date == 13].index
-categories = data[date == 'juin-10'].iloc[:,1]
-for a_voir in a_regarder:
-    print("probleme pour ", a_voir)
-    extrait = data[date == a_voir].iloc[:,1]
-    manquant = ~categories.isin(extrait)
-    print(categories[manquant])
+for icol in range(2, data.shape[1]):
+    assert data.iloc[:,icol].dtype == 'float64'
 
+assert all(data.notnull())
+# a finir mais peut-être de nouvelles données viendront...
+date = data.date
+categories = data.sujet.unique()
+assert all(data.date.value_counts() == len(categories))
+# en cas de soucis
+#count_by_date = date.value_counts()
+#a_regarder = count_by_date[count_by_date == 13].index
+#for a_voir in a_regarder:
+#    print("probleme pour ", a_voir)
+#    extrait = data[date == a_voir].iloc[:,1]
+#    manquant = ~categories.isin(extrait)
+#    print(categories[manquant])
 
 # change les dates
 mois_to_num = dict(
@@ -53,13 +59,19 @@ mois_to_num = dict(
 for mois, num in mois_to_num.items():
     date = date.str.replace(mois + '.-', num + '-')
     date = date.str.replace(mois + '.', num + '-')
-data.iloc[:,0] = date
+
+date.replace('09-mbre-10', '09-10', inplace=True)
+assert all(date.str.len() == 5)
+date = pd.to_datetime(date, format='%m-%y')
+data.index = pd.DatetimeIndex(date).normalize()
+del data['date']
+
+# pourcentage par sujet 
+pourcent_by_sujet = data.copy()
+Total = data['Totaux']
+del data['Totaux']
 
 
-data.iloc[:,1][~data.iloc[:,1].isin(categories.values)].value_counts()
 
-data.groupby(date)['Totaux'].sum()
-
-data.columns = ['date', 'sujet'] + data.columns.tolist()[2:]
 # on fait les brutes pour l'instant...
 data.to_csv('data/durees2.csv', index=False)
